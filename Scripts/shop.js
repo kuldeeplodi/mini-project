fd734940f9b29c349496867ab48aae393d92a239
Aman Singh <singhaman38809@gmail.com>
2023-12-21 16:15:54 +0530

    Add Shoes in Shop From Product.js

--- Scripts/shop.js
+++ Scripts/shop.js
@@ -1,10 +1,38 @@
-let Product = document.getElementsByClassName("pro");
+import { Products, newlyLaunchShoes } from "../data/Products.js";
 
-Product.forEach(element => {
-  element.addEventListener('click',()=>{
+const  allProducts = [...Products, ...newlyLaunchShoes] ;
+console.log(allProducts)
+let shopHTML ="";
+allProducts.forEach((Product) => {
+  shopHTML +=
+    `
+     <div class="pro">
+          <img src="${Product.image}"/>
+          <div class="des">
+            <span>${Product.name}</span>
+            <h5>High Quality Shoes</h5>
+             <img class="product-rating stars" src="images/rating/rating-${Product.rating * 10}.png">
+            <h4>RS ${Product.price}</h4>
+          </div>
+          <a href="#" class="cart"><i class="fa-solid fa-cart-shopping"></i></a>
+      </div>
+  `
+});
+
+document.querySelector(".js-allShoes-container").innerHTML = shopHTML ; 
+
+let product = document.querySelectorAll(".pro");
+product.forEach((Shoes)=>{
+  Shoes.addEventListener('click', () => {
     window.location.href = "singlepro.html";
   })
-});
+})
+
+// Product.forEach(element => {
+//   element.addEventListener('click',()=>{
+//     window.location.href = "singlepro.html";
+//   })
+// });
 // Product[0].addEventListener("click", () => {
 //   window.location.href = "singlepro.html";
 // });
